8c2c3259e9d1b88f4e2219c67b11717ac03c7812
Ayush <ayushshivhare51@gmail.com>
2025-11-05 19:15:53 +0530

    feat: peerjs

---
+++ client/src/components/Call/Call.tsx
@@ -0,0 +1,78 @@
+import { useEffect, useRef, useState } from "react";
+import Peer from "peerjs";
+
+const Call = () => {
+  const [peerId, setPeerId] = useState<string>("");
+  const [remotePeerIdValue, setRemotePeerIdValue] = useState<string>("");
+
+  const remoteVideoRef = useRef(null);
+  const currentUserVideoRef = useRef(null);
+  const peerInstance = useRef(null);
+
+  useEffect(() => {
+    const peer = new Peer();
+
+    peer.on("open", (id) => {
+      setPeerId(id);
+    });
+
+    peer.on("call", (call) => {
+      navigator.mediaDevices
+        .getUserMedia({ video: true, audio: true })
+        .then((mediaStream) => {
+          currentUserVideoRef.current.srcObject = mediaStream;
+          currentUserVideoRef.current.play();
+          call.answer(mediaStream);
+          call.on("stream", (remoteStream) => {
+            remoteVideoRef.current.srcObject = remoteStream;
+            remoteVideoRef.current.play();
+          });
+        })
+        .catch((error) => {
+          console.error("Error accessing media devices:", error);
+        });
+    });
+    peerInstance.current = peer;
+  }, []);
+
+  const call = (remotePeerId) => {
+    navigator.mediaDevices
+      .getUserMedia({ video: true, audio: true })
+      .then((mediaStream) => {
+        currentUserVideoRef.current.srcObject = mediaStream;
+        currentUserVideoRef.current.play();
+
+        const call = peerInstance.current.call(remotePeerId, mediaStream);
+
+        call.on("stream", (remoteStream) => {
+          remoteVideoRef.current.srcObject = remoteStream;
+          remoteVideoRef.current.play();
+        });
+      })
+      .catch((error) => {
+        console.error("Error accessing media devices:", error);
+      });
+  };
+
+  return (
+    <>
+      <div className="App">
+        <h1>Current user id is {peerId}</h1>
+        <input
+          type="text"
+          value={remotePeerIdValue}
+          onChange={(e) => setRemotePeerIdValue(e.target.value)}
+        />
+        <button onClick={() => call(remotePeerIdValue)}>Call</button>
+        <div>
+          <video ref={currentUserVideoRef} />
+        </div>
+        <div>
+          <video ref={remoteVideoRef} />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Call;
